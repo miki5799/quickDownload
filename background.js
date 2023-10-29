058f88531695247c3a2ff5759e72fc18f21644f9
Hailu, Mikael <mikael.hailu@stud.tu-darmstadt.de>
2023-10-29 12:52:02 +0100

    minor bug fixes

--- background.js
+++ background.js
@@ -13,52 +13,71 @@ function logDebugMessage(message) {
   });
 }
 
+function suggestFilename(path, suggest) {
+  suggest(
+    { filename: path, conflictAction: "uniquify" },
+    (downloadId) => {
+      if (chrome.runtime.lastError) {
+        console.error("error: ", chrome.runtime.lastError);
+        console.warn(
+          `An error occurred: ${chrome.runtime.lastError.message} and ${downloadId}`
+        );
+        return false;
+      } else {
+        console.log(`Download started with ID: ${downloadId}`);
+        return true;
+      }
+    }
+  );
+}
+
+const defaultPath = "Downloads/";
+
 chrome.downloads.onDeterminingFilename.addListener((downloadItem, suggest) => {
   try {
     chrome.storage.local.get(["domainRegistry", "debugMode"], (data) => {
-      const registry = data.domainRegistry;
-      console.log(registry);
-      const debugMode = data.debugMode;
-      const domainUrl = new URL(downloadItem.referrer);
-      console.log(downloadItem);
-      console.log(domainUrl);
-      const domain = new URL(downloadItem.referrer).href;
-      console.log(domain);
-
-      console.log(`Download recognized from ${domain}`);
-      console.log(`Checking if website ${domain} is in registry...`);
-
-      var newFilename = "";
-      if (registry[domain]) {
-        console.log(
-          `Website ${domain} found in registry. Redirecting download to ${registry[domain]}`
-        );
-        newFilename = registry[domain] + downloadItem.filename;
-      } else {
-        console.log(
-          `Website ${domain} not found in registry. Using default download path.`
-        );
-        newFilename = "Downloads/" + downloadItem.filename;
-      }
-      console.log(newFilename);
-      console.log(typeof newFilename);
-      console.log(downloadItem.finalUrl);
-
-      suggest(
-        { filename: newFilename, conflictAction: "uniquify" },
-        (downloadId) => {
-          if (chrome.runtime.lastError) {
-            console.error("error: ", chrome.runtime.lastError);
-            console.warn(
-              `An error occurred: ${chrome.runtime.lastError.message} and ${downloadId}`
-            );
-            return false;
-          } else {
-            console.log(`Download started with ID: ${downloadId}`);
-            return true;
-          }
+      try {
+        const registry = data.domainRegistry;
+        console.log(registry);
+        const debugMode = data.debugMode;
+        console.log(downloadItem.referrer);
+        try {
+          const domainUrl = new URL(downloadItem.referrer);
+        } catch (error) {
+          console.log(error);
+          suggestFilename(defaultPath + downloadItem.filename, suggest);
+          return true;
         }
-      );
+        console.log(downloadItem);
+        console.log(domainUrl);
+        const domain = new URL(downloadItem.referrer).href;
+        console.log(domain);
+
+        console.log(`Download recognized from ${domain}`);
+        console.log(`Checking if website ${domain} is in registry...`);
+
+        var newFilename = "";
+        if (registry[domain]) {
+          console.log(
+            `Website ${domain} found in registry. Redirecting download to ${registry[domain]}`
+          );
+          newFilename = registry[domain] + downloadItem.filename;
+        } else {
+          console.log(
+            `Website ${domain} not found in registry. Using default download path.`
+          );
+          newFilename = "Downloads/" + downloadItem.filename;
+        }
+        console.log(newFilename);
+        console.log(typeof newFilename);
+        console.log(downloadItem.finalUrl);
+
+        suggestFilename(newFilename, suggest);
+      } catch (error) {
+        console.error(error);
+        suggestFilename(defaultPath + downloadItem.filename, suggest);
+        return true;
+      }
     });
     return true;
   } catch (error) {
